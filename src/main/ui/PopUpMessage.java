a1707496a6309d727fabf1de6b77057aabd649b3
James Wen <leranjamesw@gmail.com>
2023-03-31 00:01:23 -0700

    Added More comments.

--- src/main/ui/PopUpMessage.java
+++ src/main/ui/PopUpMessage.java
@@ -2,6 +2,7 @@ package ui;
 
 import javax.swing.*;
 
+// Represents a popup message
 public class PopUpMessage {
     public PopUpMessage(String message) {
         JOptionPane.showMessageDialog(null, message);
